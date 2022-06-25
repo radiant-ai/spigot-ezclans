67fa1bf7a271dc001fe12030bfcd0089567797c9
radiant-ai <bodia1206@gmail.com>
2022-06-24 20:26:38 -0500

    rewrite pvp handling

--- src/main/java/com/github/radiant/ezclans/events/PvpEvent.java
+++ src/main/java/com/github/radiant/ezclans/events/PvpEvent.java
@@ -1,68 +1,49 @@
 package com.github.radiant.ezclans.events;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.ThrownPotion;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.projectiles.ProjectileSource;
 
-import com.github.radiant.ezclans.core.ClanMember;
 import com.github.radiant.ezclans.core.Clans;
 
 public class PvpEvent implements Listener {
 	
+	//If player receives damage from another player and they are both in same clan and they have clan pvp disabled, cancel the damage.
 	@EventHandler(priority = EventPriority.HIGH)
-    public void EntityDamageByEntityEvent(final EntityDamageByEntityEvent event) {
-		Player attackingPlayer = null;
-        Player attackedPlayer = null;
-        if (event.getEntity() instanceof Player) {
-            Entity damager = event.getDamager();
-            attackingPlayer = null;
-            attackedPlayer = (Player) event.getEntity();
-            if (damager instanceof Arrow) {
-            	Arrow arrow = (Arrow) damager;
-            	ProjectileSource ps = arrow.getShooter();
-            	if (ps instanceof Player) {
-            		attackingPlayer = (Player) ps;
-            	}
-            }
-            else if (damager instanceof ThrownPotion) {
-            	ThrownPotion potion = (ThrownPotion) damager;
-            	ProjectileSource ps = potion.getShooter();
-            	if (ps instanceof Player) {
-            		attackingPlayer = (Player) ps;
-            	}
-            }
-            else if (damager instanceof Player) {
-                attackingPlayer = (Player) damager;
-                if (!(event.getEntity() instanceof Player)) {
-                	return;
-                }
-            }
-            else {
-            	return;
-            }
-            if (attackingPlayer!=null && attackedPlayer!=null) {
-            	ClanMember attacked = Clans.getMember(attackedPlayer.getUniqueId());
-            	if (attacked == null) {
-            		return;
-            	}
-            	ClanMember attacking = Clans.getMember(attackingPlayer.getUniqueId());
-            	if (attacking == null) {
-            		return;
-            	}
-            	if (attacked.getClan() != attacking.getClan()) {
-            		return;
-            	}
-            	if (!attacked.isClanPvp() || !attacking.isClanPvp()) {
-            		event.setCancelled(true);
-            	}
-            }
-        }
-    }
+	public void EntityDamageByEntityEvent(final EntityDamageByEntityEvent event) {
+		if (!(event.getEntity() instanceof Player attackedPlayer)) {
+			return;
+		}
+		Player attacker = null;
+
+		if (event.getDamager() instanceof Player attackingPlayer) {
+			attacker = attackingPlayer;
+		}
+
+		if (event.getDamager() instanceof Projectile projectile &&
+		projectile.getShooter() instanceof Player shootingPlayer) {
+			attacker = shootingPlayer;
+		}
+
+		if (attacker == null) {
+			return;
+		}
+
+		if (!shouldCancelPvp(attacker, attackedPlayer)) {
+			return;
+		}
+
+		event.setCancelled(true);
+	}
+
+	private boolean shouldCancelPvp(Player player1, Player player2) {
+		var member1 = Clans.getMember(player1.getUniqueId());
+		var member2 = Clans.getMember(player2.getUniqueId());
+		if (member1.getClan() != member2.getClan()) {
+			return false;
+		}
+		return !member1.isClanPvp() || !member2.isClanPvp();
+	}
 }
